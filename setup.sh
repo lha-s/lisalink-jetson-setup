313166c49e47bacc77149dc4a3e309d141ac3f91
Mulham Soufi <soufimulham@hotmail.fr>
2021-08-18 13:38:12 +0200

    edit config.json + fan

--- setup.sh
+++ setup.sh
@@ -3,23 +3,23 @@
 # >>>>>>>>>>>> sudo setup.sh <<<<<<<<<<<
 # Installation tested for Jetson Jetpack 4.5.1
 
-# step 0, dependencies
+# dependencies
 sudo apt install python3-pip
 sudo apt-get install -y libffi-dev
 sudo apt-get install -y python-openssl
 sudo apt-get install libssl-dev
 sudo apt-get install curl
 
-# step 1, install Python PIP
+# install Python PIP
 sudo apt-get update -y
 sudo apt-get install -y curl
 curl -sSL https://bootstrap.pypa.io/get-pip.py | sudo python
 
-# step 2, install Docker Compose build dependencies
+# install Docker Compose build dependencies
 sudo apt-get install -y libffi-dev
 sudo apt-get install -y python-openssl
 
-# step 3, install Docker Compose via pip (latest= 1.26.0?)
+# install Docker Compose via pip (latest= 1.26.0?)
 export DOCKER_COMPOSE_VERSION=1.24.0
 sudo pip install docker-compose=="${DOCKER_COMPOSE_VERSION}"
 
@@ -38,3 +38,30 @@ chmod 777 install-opendatacam.sh
 ./install-opendatacam.sh --platform nano
 
 sudo docker-compose stop
+
+# Edit configuration file config.json to keep only "person"
+sed -i 's/TO_REPLACE_VIDEO_INPUT/usbcam/' config.json
+
+sed -e '/bicycle/ s/^#*/#/g' -i config.json
+sed -e '/truck/ s/^#*/#/g' -i config.json
+sed -e '/motorbike/ s/^#*/#/g' -i config.json
+sed -e '/car/ s/^#*/#/g' -i config.json
+sed -e '/bus/ s/^#*/#/g' -i config.json
+
+sed -i 's/.*VALID_CLASSES.*/  "VALID_CLASSES": ["person"],/' config.json
+
+# Run fan (max speed 255)
+#sudo sh -c 'echo 100 > /sys/devices/pwm-fan/target_pwm' 
+
+# Run fan on reboot automatically
+touch /etc/rc.local
+echo "#!/bin/bash" >> /etc/rc.local
+echo "sleep 10" >> /etc/rc.local
+echo "sudo /usr/bin/jetson_clocks" >> /etc/rc.local
+echo "sudo sh -c 'echo 100 > /sys/devices/pwm-fan/target_pwm'" >> /etc/rc.local
+
+sudo chmod u+x /etc/rc.local
+
+#reboot
+sudo reboot
+
