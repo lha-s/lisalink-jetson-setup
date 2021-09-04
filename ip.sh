431e6939ebd94330a7b1c62003a58cab7b810ddd
Mulham Soufi <soufimulham@hotmail.fr>
2021-09-04 12:22:58 +0200

    ip

--- ip.sh
+++ ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'curl -d "name=$HOME&ip=$(hostname -I)" -X POST http://api.mohamedelkadaoui.fr/devices' >> /etc/rc.local
+curl -d "name=$HOME&ip=$(hostname -I)" -X POST http://api.mohamedelkadaoui.fr/devices
