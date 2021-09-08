51d65902673b20230c9aa544b113ee06e0be442a
Mulham Soufi <soufimulham@hotmail.fr>
2021-09-08 18:49:42 +0200

    fix: bad path used in reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-# catch user logname
-cp ./user.sh ~/
-runuser -l  lisalink -c '~/user.sh'
-
-user=$(<~/.logname)
-
-sudo echo "sudo /home/$user/lisalink-setup/docker-compose restart" >> /etc/rc.local
+sudo echo "sudo $PWD/lisalink-setup/docker-compose restart" >> /etc/rc.local
